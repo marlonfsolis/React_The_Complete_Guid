326fbcf31e8a4e15822680785294336f6c7f4423
Marlon Fernandez <marlonfsolis@outlook.com>
2020-01-25 10:20:29 -0500

    change person name

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PersonModel from './models/Person';
-import Person from './components/Person/Person';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import PersonModel from "./models/Person";
+import Person from "./components/Person/Person";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-
   const [personState, setPersonState] = useState({
-    persons: [
-      new PersonModel(1, 'Marlon', 34),
-      new PersonModel(2, 'Yenni', 27),
-      new PersonModel(3, 'Ivan', 83),
-      new PersonModel(4, 'Luisa', 70)
-    ]
+    persons: [new PersonModel(1, "Marlon", 34), new PersonModel(2, "Yenni", 27), new PersonModel(3, "Ivan", 83), new PersonModel(4, "Luisa", 70)]
   });
   const [showPersonState, setShowPersonState] = useState(true);
 
@@ -28,23 +23,44 @@ function App() {
     setPersonState({ persons });
   };
 
+  const changePersonNameHandler = (id, event) => {
+    let persons = [...personState.persons];
+    let personIndex = persons.findIndex((p) => {
+      return p.id === id;
+    });
+    let person = { ...persons[personIndex] };
+    person.name = event.currentTarget.value;
+    persons[personIndex] = person;
+
+    setPersonState({persons});
+  };
+
   let persons = null;
   if (showPersonState) {
     persons = (
       <div>
-        {
-          personState.persons.map(person => {
-            return (
-              <Person key={person.id} name={person.name} age={person.age} deletePersonHandler={deletePersonHandler.bind(this, person.id)} />
-            );
-          })
-        }
+        {personState.persons.map((person) => {
+          return (
+            <Person
+              key={person.id}
+              name={person.name}
+              age={person.age}
+              deletePersonHandler={deletePersonHandler.bind(this, person.id)}
+              changePersonNameHandler={changePersonNameHandler.bind(this, person.id)}
+            />
+          );
+        })}
       </div>
     );
   }
 
   return (
     <div className="App">
+      <Row>
+        <Col sm="3"></Col>
+        <Col sm="6">{persons}</Col>
+        <Col sm="3"></Col>
+      </Row>
       {persons}
     </div>
   );
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Row, Col } from "react-bootstrap";
 
 export default function Person(props) {
-    return (
-        <div>
-            <Card style={{ margin: '10px' }}>
-                <Card.Body>
-                    <Card.Title>{props.name}</Card.Title>
-                    <Card.Text>
-                        I am {props.age} years old.
-                    </Card.Text>
-                    <Button variant="danger" onClick={props.deletePersonHandler}>Delete</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card style={{ margin: "10px" }}>
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          <p>I am {props.age} years old.</p>
+          <Form>
+              <Form.Group as={Row}>
+                  <Form.Label column sm="3">New Name</Form.Label>
+                  <Col sm="9">
+                      <Form.Control type="text" defaultValue={props.name}></Form.Control>
+                  </Col>
+              </Form.Group>
+          </Form>
+          <Button className="mr-2" variant="danger" onClick={props.deletePersonHandler}>
+            Delete
+          </Button>
+          <Button variant="success" onClick={props.changePersonNameHandler}>
+            Change Name
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
