aea7e8d7e0cf0a55fbc51dd7918f75a55759acb4
Marlon Fernandez <marlonfsolis@outlook.com>
2020-01-25 19:51:37 -0500

    Form in cards not finish

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import PersonModel from "./models/Person";
 import Person from "./components/Person/Person";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  // ** states ** //
+
   const [personState, setPersonState] = useState({
     persons: [new PersonModel(1, "Marlon", 34), new PersonModel(2, "Yenni", 27), new PersonModel(3, "Ivan", 83), new PersonModel(4, "Luisa", 70)]
   });
   const [showPersonState, setShowPersonState] = useState(true);
 
-  const deletePersonHandler = (id, event) => {
+  const [form, setForm] = useState({
+    name: { value: "Pepe" }
+  });
+
+  const setFormFieldVal = (name, event) => {
+    let myForm = { ...form };
+    myForm[name].value = event.currentTarget.value;
+    setForm(myForm);
+  };
+
+  // ** funtions ** //
+
+  const deletePersonHandler = (id) => {
     let persons = [...personState.persons];
     persons = persons.filter((p) => {
       if (p.id !== id) {
@@ -23,17 +38,23 @@ function App() {
     setPersonState({ persons });
   };
 
-  const changePersonNameHandler = (id, event) => {
+  const changePersonNameHandler = (id, name) => {
     let persons = [...personState.persons];
     let personIndex = persons.findIndex((p) => {
       return p.id === id;
     });
     let person = { ...persons[personIndex] };
-    person.name = event.currentTarget.value;
+    person.name = name;
     persons[personIndex] = person;
 
-    setPersonState({persons});
+    setPersonState({ persons });
   };
+
+  const tooglePersonList = () => {
+    setShowPersonState(!showPersonState);
+  };
+
+  // ** View variables ** //
 
   let persons = null;
   if (showPersonState) {
@@ -45,6 +66,8 @@ function App() {
               key={person.id}
               name={person.name}
               age={person.age}
+              form={form}
+              setFormFieldVal={setFormFieldVal.bind(this)}
               deletePersonHandler={deletePersonHandler.bind(this, person.id)}
               changePersonNameHandler={changePersonNameHandler.bind(this, person.id)}
             />
@@ -57,11 +80,15 @@ function App() {
   return (
     <div className="App">
       <Row>
+        <Col>
+          <Button onClick={tooglePersonList.bind(this)}>Toogle Person List</Button>
+        </Col>
+      </Row>
+      <Row>
         <Col sm="3"></Col>
         <Col sm="6">{persons}</Col>
         <Col sm="3"></Col>
       </Row>
-      {persons}
     </div>
   );
 }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -5,6 +5,13 @@ import Form from "react-bootstrap/Form";
 import { Row, Col } from "react-bootstrap";
 
 export default function Person(props) {
+
+  const form = props.form;
+
+  const setFormFieldValue = (name, event) => {
+    props.setFormFieldVal(name, event);
+  };
+
   return (
     <div>
       <Card style={{ margin: "10px" }}>
@@ -12,17 +19,19 @@ export default function Person(props) {
           <Card.Title>{props.name}</Card.Title>
           <p>I am {props.age} years old.</p>
           <Form>
-              <Form.Group as={Row}>
-                  <Form.Label column sm="3">New Name</Form.Label>
-                  <Col sm="9">
-                      <Form.Control type="text" defaultValue={props.name}></Form.Control>
-                  </Col>
-              </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column sm="3">
+                New Name
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control type="text" value={form['name'].value} onChange={setFormFieldValue.bind(this, 'name')}></Form.Control>
+              </Col>
+            </Form.Group>
           </Form>
           <Button className="mr-2" variant="danger" onClick={props.deletePersonHandler}>
             Delete
           </Button>
-          <Button variant="success" onClick={props.changePersonNameHandler}>
+          <Button variant="success">
             Change Name
           </Button>
         </Card.Body>
